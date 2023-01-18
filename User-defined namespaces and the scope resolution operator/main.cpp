7c9e5077cdb3801d6df967be757ebc3fa446e034
unknown <dogethanthanh@gmail.com>
2023-01-18 18:26:18 +0700

    C++ 18/1/2023

---
+++ User-defined namespaces and the scope resolution operator/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace foo{
+    void doSmth(){
+        std::cout<<"hi hello how are you"<<'\n';
+    }
+}
+
+int main(){
+    foo::doSmth();
+    return 0;    
+}
---
+++ size_t/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cstdint>
+int main(){
+    float a=5.3f;
+    std::cout<< static_cast<int>(a)<<"\n";
+    return 0;
+}
