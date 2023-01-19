bd95053b1663c2fdbf3c503ce4728392906e1c9d
Thu Phương Hoàng <dogethanthanh@gmail.com>
2023-01-19 14:38:05 +0700

    learning oop

--- oop/main.cpp
+++ oop/main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
-class Employ
-{
-public:
-int m_year{};
-int m_month{};
-int m_day{};
+int main(){
 
-void print(){
-    std::cout<<"Year:  "<<m_year<<"  "<<"Month:  "<<m_month<<'\n';
-}
-private:
-
-};
-int main();
-    Employ joe {2023,4,14};
-    joe.print();
-    return 0;
+    return 0;   
 }
 
 
