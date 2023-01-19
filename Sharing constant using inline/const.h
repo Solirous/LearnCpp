ed76b98c843fb6ed50b60ced634a0f6ae2785ec0
Thu Phương Hoàng <dogethanthanh@gmail.com>
2023-01-19 14:36:40 +0700

    learning oop

---
+++ Sharing constant using inline/const.h
@@ -0,0 +1,9 @@
+#ifndef CONST
+#define CONST
+
+namespace cons{
+    constexpr double pi {3.14};
+    constexpr double gravity {9.8};
+}
+
+#endif
---
+++ Sharing constant using inline/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "const.h"
+
+int main(){
+    std::cout<<"The gravity of the earth is:  "<< cons::gravity<<'\n';
+    return 0;
+}
